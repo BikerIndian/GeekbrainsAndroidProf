4812ee3f7e7b436dea6bbdd03cd636805b283b64
Vladimir Svishch <5693031@gmail.com>
2021-04-23 22:25:55 +0300

    Урок 3. Koin и корутины 1. Используйте в своем проекте Koin и Coroutines.

--- app/src/main/java/net/svishch/android/dictionary/model/repository/RetrofitImplementation.kt
+++ app/src/main/java/net/svishch/android/dictionary/model/repository/RetrofitImplementation.kt
@@ -1,19 +1,20 @@
 package net.svishch.android.dictionary.model.repository
-
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import net.svishch.android.dictionary.model.repository.entity.DataModel
 import io.reactivex.Observable
 import net.svishch.android.dictionary.model.datasource.BaseInterceptor
 import net.svishch.android.dictionary.model.datasource.DataSource
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.CallAdapter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitImplementation : DataSource<List<DataModel>> {
 
-    override fun getData(word: String): Observable<List<DataModel>> {
-        return getService(BaseInterceptor.interceptor).search(word)
+    override suspend fun getData(word: String): List<DataModel> {
+        return getService(BaseInterceptor.interceptor).searchAsync(word).await()
     }
 
     private fun getService(interceptor: Interceptor): ApiService {
@@ -24,7 +25,7 @@ class RetrofitImplementation : DataSource<List<DataModel>> {
         return Retrofit.Builder()
             .baseUrl(BASE_URL_LOCATIONS)
             .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .client(createOkHttpClient(interceptor))
             .build()
     }
@@ -34,7 +35,7 @@ class RetrofitImplementation : DataSource<List<DataModel>> {
         httpClient.addInterceptor(interceptor)
 
         // Вывод лога полученных данных в консоль
-        // httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
         return httpClient.build()
     }
 
--- app/src/main/java/net/svishch/android/dictionary/view/main/MainInteractor.kt
+++ app/src/main/java/net/svishch/android/dictionary/view/main/MainInteractor.kt
@@ -4,21 +4,20 @@ import net.svishch.android.dictionary.viewmodel.Interactor
 import net.svishch.android.dictionary.model.AppState
 import net.svishch.android.dictionary.model.repository.entity.DataModel
 import net.svishch.android.dictionary.model.repository.Repository
-import io.reactivex.Observable
-import net.svishch.android.dictionary.di.NAME_LOCAL
-import net.svishch.android.dictionary.di.NAME_REMOTE
-import javax.inject.Named
+
 
 class MainInteractor(
-    @Named(NAME_REMOTE) val repositoryRemote: Repository<List<DataModel>>,
-    @Named(NAME_LOCAL) val repositoryLocal: Repository<List<DataModel>>
+    private val repositoryRemote: Repository<List<DataModel>>,
+    private val repositoryLocal: Repository<List<DataModel>>
 ) : Interactor<AppState> {
 
-    override fun getData(word: String, fromRemoteSource: Boolean): Observable<AppState> {
-        return if (fromRemoteSource) {
-            repositoryRemote
-        } else {
-            repositoryLocal
-        }.getData(word).map { AppState.Success(it) }
+    override suspend fun getData(word: String, fromRemoteSource: Boolean): AppState {
+        return AppState.Success(
+            if (fromRemoteSource) {
+                repositoryRemote
+            } else {
+                repositoryLocal
+            }.getData(word)
+        )
     }
 }
